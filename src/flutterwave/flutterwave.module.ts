dd70eacc52fc7c67a3761300ffc04108446340da
Ayomide Kofoworola <ayomidekofoworola59@gmail.com>
2026-07-28 13:05:53 +0100

    Fix: fix pwa page

--- src/flutterwave/flutterwave.module.ts
+++ src/flutterwave/flutterwave.module.ts
@@ -9,11 +9,13 @@ import { FlutterwaveService } from './flutterwave.service';
 import { WalletService } from 'src/wallet/wallet.service';
 import { WalletController } from 'src/wallet/wallet.controller';
 import { NotificationModule } from 'src/notification/notification.module';
+import { DispatchModule } from 'src/dispatch/dispatch.module'; // 👈 1. Import DispatchModule
 
 @Module({
   imports: [
     ConfigModule,
     NotificationModule,
+    DispatchModule, // 👈 2. Add DispatchModule to imports array
     HttpModule.register({
       timeout: 30000,
       maxRedirects: 5,
@@ -25,6 +27,6 @@ import { NotificationModule } from 'src/notification/notification.module';
     PrismaService,
     WalletService,
   ],
-  exports: [FlutterwaveService,WalletService],
+  exports: [FlutterwaveService, WalletService],
 })
 export class FlutterwaveModule {}
